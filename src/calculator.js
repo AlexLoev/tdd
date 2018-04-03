19889b9d4dd72ef757ae200b111ca9ce8816ca1b
AlexLoev <alexloev1988@gmail.com>
2018-04-04 00:04:39 +0300

    made all tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,50 @@
 function add(numbers) {
     if (typeof numbers == 'string') {
+        var err = new Error;
         var sum = 0;
-        var arrayofnum = [];
         try {
-            arrayofnum = numbers
-                .replace(/\n/g,',')
-                .split(',');
-            arrayofnum.forEach(element => {
-                let i = parseInt(element)
-                // console.log(i, typeof i, isNaN(i));
+            var template = numbers.match(/\/\/(.*)\n(.*)/); // разбираем шаблон на 2 строки {//[разделители]\n[числа]}
+            var strNumbers = '';
+            var arSplitters = [];
+            var arNumbers = [];
+            if (template) {
+                arSplitters = template[1].match(/<(.*?)>/g); //собираем все разделители
+                strNumbers = template[2];
+                arSplitters.forEach(splitter => {
+                    let s = "\\" + splitter.match(/<(.*?)>/)[1];
+                    let regex = new RegExp(s, "g");
+                    strNumbers = strNumbers.replace(regex, ',');
+                });
+            } else {
+                arSplitters = ',';
+                strNumbers = numbers.replace(/\n/g, ',')
+            }
+            arNumbers = strNumbers.split(',');
+            
+            arNumbers.forEach(number => {
+                let i = parseInt(number)
                 if (typeof i == 'number' & !isNaN(i)) {
-                    sum += i;
+                    if (i < 0) {
+                        err.message += ' ' + i;
+                    } else if (i < 1000) {
+                        sum += i;
+                    }
                 }
             });
         } catch (error) {
             sum = 0;
         }
 
+        if (err.message) {
+            throw err;
+        } else {
+            return sum;
+        }
 
-        // console.log(arrayofnum, sum);
-        return sum;
-        
     } else {
         return 0
     };
 };
 
-// add('1\n2,3');
-module.exports = {add};
+module.exports = { add };
+
--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -12,23 +12,40 @@ describe('testing calculator', () => {
 });
 
 describe('testing calculator.add(numbers)', () => {
-    it('should return 0 if argument is undefined', () => {
+    it('should return 0 if arg undefined', () => {
         expect(calculator.add()).equal(0);
     });
-    it('should return 6 if argument "1,2,3"', () => {
+    it('should return 6 if arg "1,2,3"', () => {
         expect(calculator.add('1,2,3')).equal(6);
     });
-    it('should return 5 if args " ,2,3"', () => {
+    it('should return 5 if arg " ,2,3"', () => {
         expect(calculator.add(' ,2,3')).equal(5);
     });
-    it('should return 45 if args "1,2,3,4,5,6,7,8,9"', () => {
+    it('should return 45 if arg "1,2,3,4,5,6,7,8,9"', () => {
         expect(calculator.add('1,2,3,4,5,6,7,8,9')).equal(45);
     });
-    it('should return 6 if args "1\\n2,3"', () => {
+    it('should return 6 if arg "1\\n2,3"', () => {
         expect(calculator.add('1\n2,3')).equal(6);
     });
-    it('should return 1 if args "1,\\n"', () => {
+    it('should return 1 if arg "1,\\n"', () => {
         expect(calculator.add('1,\n')).equal(1);
     });
-
+    it('should accept any splitter if input template like //<splitter>\\n[numbers…] and return 3 if arg "//<;>\\n1;2"', () => {
+        expect(calculator.add('//<;>\n1;2')).equal(3);
+    });
+    it('should throw an Error if negative numbers and return all negative numbers', () => {
+        expect(calculator.add.bind(calculator, '-1,1,-2')).to.throw(' -1 -2');
+    });
+    it('should ignore numbers greater than 1000 and return 4 if arg "1,1000,3,5000"', () => {
+        expect(calculator.add('1,1000,3,5000')).equal(4);
+    });
+    it('should accept any length of splitter and return 6 if arg "//<ggg>\\n1ggg2ggg3"', () => {
+        expect(calculator.add('//<ggg>\n1ggg2ggg3')).equal(6);
+    });
+    it('should accept different splitters in one input template "//<разделитель1><разделитель2>\\n" and return 6 if arg "//<*><%>\\n1*2%3"', () => {
+        expect(calculator.add('//<*><%>\n1*2%3')).equal(6);
+    });
+    it('should accept many many splitters in one input template "//<разделитель1><разделитель2>\\n" and return 15 if arg "//<*><%><ggg><tttttttttttttttttttt>\\n1*2%3ggg4ggg5tttttttttttttttttttt6', () => {
+        expect(calculator.add('//<*><%><ggg><tttttttttttttttttttt>\n1*2%3ggg4ggg5tttttttttttttttttttt6')).equal(15);
+    });
 });
